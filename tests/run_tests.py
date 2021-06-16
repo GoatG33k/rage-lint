6b9ead8e811ffddd1a09273e74d83416f573b69e
GoatGeek <goatgeek@relta.net>
2021-06-15 23:49:56 -0700

    chore: better testing for all car schemas, weapons

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -21,17 +21,23 @@ with open(os.devnull, 'wb') as null:
         cmd_parts = (sys.executable, script_path, test_file)
         rc = 0
         try:
-            rc = subprocess.check_call(cmd_parts, env=os.environ, stdout=null )
+            rc = subprocess.check_call(cmd_parts, env=os.environ, stdout=null)
         except subprocess.CalledProcessError as e:
             rc = e.returncode
 
         # test return code
         if (not expected_to_fail and rc > 0) or (expected_to_fail and rc == 0):
-            print("%sTest '%s' FAILED! Expected %s%s, but it was not...%s" % (
-                fg('red'), test_name, expected_state_str,fg('red'), attr(0)))
+            print("=> %sTest '%s' FAILED! Expected %s%s, but it was not...%s" % (
+                fg('red'), test_name, expected_state_str, fg('red'), attr(0)))
             failed = True
         else:
-            print("%sTest '%s' PASSED!%s" % (fg('green'), test_name, attr(0)))
+            print("=> %sTest '%s' PASSED!%s" % (fg('green'), test_name, attr(0)))
 
+code = 0
+result_str = "=>\n=> %sAll tests passed! :)%s" % (fg('green'), attr(0))
 if failed:
-    sys.exit(1)
+    result_str = "=>\n=> %sTests failed :(%s" % (fg('red'), attr(0))
+    code = 1
+    
+print(result_str)
+sys.exit(code)
