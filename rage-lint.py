3a39e8db376a0a8e540317d4890fb8ca2adb4fd0
GoatGeek <goatgeek@relta.net>
2021-06-19 14:26:13 -0700

    feat: version checking, version 0.0.4

--- rage-lint.py
+++ rage-lint.py
@@ -1,26 +1,18 @@
-import argparse
-import glob
-import os.path
 import sys
-import time
+from argparse import ArgumentParser
+from glob import glob
+from os import stat, unlink, getcwd
 from os.path import abspath, realpath, dirname, exists
+from time import time
 from urllib import request
 
 from colored import fg, attr
 from lxml import etree
+from packaging.version import parse as parse_version
 
-__VERSION__ = '0.0.3'
+__VERSION__ = '0.0.4'
 
-
-def silly_log(s):
-    print(s) if args.verbose >= 2 else None
-
-
-def debug(s):
-    print(s) if args.verbose >= 1 else None
-
-
-argParser = argparse.ArgumentParser(description='A linter for RAGE metafiles')
+argParser = ArgumentParser(description='A linter for RAGE metafiles')
 argParser.add_argument('globs', metavar='glob', type=str, nargs='+', help="glob paths to lint")
 argParser.add_argument('-u', '--update', action='store_true', help='forcibly update to the latest schema')
 argParser.add_argument('-v', '--verbose', action='count', default=0)
@@ -35,52 +27,67 @@ copyright_str = '\n'.join((
 ))
 print(fg('cyan') + copyright_str + attr(0))
 
-dataDir = dirname(realpath(__file__))
+try:
+    # TODO: update to master
+    version_check_url = "https://raw.githubusercontent.com/GoatG33k/rage-lint/master/latest.txt?%d" % time()
+    with request.urlopen(version_check_url) as response:
+        version_str = response.read().decode('utf-8')
+        if parse_version(__VERSION__) < parse_version(version_str):
+            print("%s-------------------------------------%s" % (fg('yellow'), attr(0)))
+            print("%sA new version %s%s%s is available!%s" % (
+                fg('green'), fg('yellow'), version_str, fg('green'), attr(0)))
+            print("https://github.com/GoatG33k/rage-lint/releases/")
+            print("%s-------------------------------------%s\n" % (fg('yellow'), attr(0)))
+except BaseException as e:
+    print("%s%s%s" % (fg('red'), e, attr(0)))
+    print("%sFailed to check for application update%s" % (fg('yellow'), attr(0)))
+
+data_dir = dirname(realpath(__file__))
 if getattr(sys, 'frozen', False):
-    dataDir = sys._MEIPASS
+    data_dir = sys._MEIPASS
 
-xsdSchema = None
-schemaPath = abspath(dataDir + '/schema.xsd')
-if exists(schemaPath):
-    schemaAge = time.time() - (os.stat(schemaPath)).st_mtime
-    # remove cached after 1 week
-    if schemaAge > (3600 * 24 * 7) or args.update:
+xsd_schema = None
+xsd_schema_path = abspath(data_dir + '/schema.xsd')
+if exists(xsd_schema_path):
+    schema_age = time() - (stat(xsd_schema_path)).st_mtime
+    # remove cached after 1 week, or if update requested
+    if schema_age > (3600 * 24 * 7) or args.update:
         print("%sRemoving old schema and re-fetching...%s" % (fg('yellow'), attr(0)))
-        os.unlink(schemaPath)
+        unlink(xsd_schema_path)
 
-if not exists(schemaPath):
-    xsdUrl = "https://raw.githubusercontent.com/GoatG33k/gta5-xsd/master/GTA5.xsd"
+if not exists(xsd_schema_path):
+    xsd_url = "https://raw.githubusercontent.com/GoatG33k/gta5-xsd/master/GTA5.xsd"
     print("%sDownloading schema...%s" % (fg('yellow'), attr(0)))
-    with request.urlopen(xsdUrl) as response, open(schemaPath, 'w') as f:
+    with request.urlopen(xsd_url) as response, open(xsd_schema_path, 'w') as f:
         f.write(response.read().decode('utf-8'))
         f.close()
 
-xsdRoot = None
-xsdSchema = None
+xsd_root = None
+xsd_schema = None
 
 try:
     print("%sReading schema...%s" % (fg('yellow'), attr(0)))
-    xsdRoot = etree.parse(schemaPath)
-    xsdSchema = etree.XMLSchema(xsdRoot)
+    xsd_root = etree.parse(xsd_schema_path)
+    xsd_schema = etree.XMLSchema(xsd_root)
 except etree.XMLSchemaParseError as e:
     print("%sFailed to parse GTA5.xsd, please report this issue to the GitHub repository%s:" % (fg('red'), attr(0)))
     print("\t%s%s%s" % (fg('yellow'), str(e), attr(0)))
     exit(1)
 
-knownRootTypes = []
-for el in xsdRoot.iter():
+known_root_types = []
+for el in xsd_root.iter():
     parent = el.getparent()
     if parent is not None and parent.tag == '{http://www.w3.org/2001/XMLSchema}schema':
-        knownRootTypes.append(el.get('name'))
+        known_root_types.append(el.get('name'))
 
 files = []
 for _glob in args.globs:
-    globFiles = glob.glob(_glob, recursive=True)
-    files.extend(globFiles)
+    glob_files = glob(_glob, recursive=True)
+    files.extend(glob_files)
 
-totalFiles = len(files)
-skippedFiles = []
-failedFiles = []
+total_file_count = len(files)
+skipped_files = []
+failed_files = []
 
 
 def handle_pass():
@@ -88,27 +95,28 @@ def handle_pass():
 
 
 def handle_fail(path, msg):
-    failedFiles.append((path, msg))
+    failed_files.append((path, msg))
     print("%s FAIL%s" % (fg('red'), attr(0)), file=sys.stderr)
     print(("  - %s" + msg + "%s") % (fg('red'), attr(0)) + "\n", file=sys.stderr)
 
 
 def handle_skip(path, msg):
-    skippedFiles.append((path, msg))
+    skipped_files.append((path, msg))
     print("%s WARN%s" % (fg('yellow'), attr(0)), file=sys.stderr)
     print(("  - %s" + msg + "%s") % (fg('yellow'), attr(0)) + "\n", file=sys.stderr)
 
 
-print("%sFound %d file%s to lint...%s\n" % (fg('cyan'), len(files), 's' if len(files) > 0 else '', attr(0)))
+print("%sFound %s%d%s file%s to lint...%s\n" % (fg('yellow'), fg('green'), len(files), fg('yellow'),
+                                                's' if len(files) > 0 else '', attr(0)))
 for file in files:
-    relative_file_path = realpath(file).replace(os.getcwd(), '.')
+    relative_file_path = realpath(file).replace(getcwd(), '.')
     print(("Linting %s%s%s" % (fg('yellow'), relative_file_path, attr(0))).ljust(75), end="", file=sys.stderr)
     try:
         doc = etree.parse(file, parser=etree.XMLParser(remove_comments=True))
         # check that the root is recognized
-        rootTagName = doc.getroot().tag
-        if rootTagName not in knownRootTypes:
-            handle_skip(file, "The root '%s' is not recognized" % rootTagName)
+        root_tag_name = doc.getroot().tag
+        if root_tag_name not in known_root_types:
+            handle_skip(file, "The root '%s' is not recognized" % root_tag_name)
             continue
         # we do a bit of magic to process R* ambiguous array types:
         # since <Item> can be anything, if a type is specified in the file,
@@ -118,58 +126,59 @@ for file in files:
             if el.tag not in ['Item', 'item']:
                 continue
             type_attr = el.attrib.get('type')
-            if type_attr is not None and 'xs:' not in type_attr and type_attr != 'NULL' and type_attr in knownRootTypes:
+            if type_attr is not None and 'xs:' not in type_attr and type_attr != 'NULL' and type_attr in known_root_types:
                 new_tag_name = "Item__" + type_attr
                 el.tag = new_tag_name
 
-        xsdSchema.assertValid(doc)
+        xsd_schema.assertValid(doc)
         handle_pass()
     except etree.XMLSyntaxError as e:
         handle_skip(file, str(e))
     except etree.DocumentInvalid as e:
         handle_fail(file, str(e))
 
-totalFailedFiles = len(failedFiles)
-totalSkippedFiles = len(skippedFiles)
-resultStr = ("%sPASSED%s" % (fg('green'), attr(0)))
-if len(failedFiles) > 0:
-    resultStr = ("%sFAIL%s" % (fg('red'), attr(0)))
+total_failed = len(failed_files)
+total_skipped = len(skipped_files)
+_result_str = ("%sPASSED%s" % (fg('green'), attr(0)))
+if len(failed_files) > 0:
+    _result_str = ("%sFAIL%s" % (fg('red'), attr(0)))
 
-totalPassedFiles = totalFiles - totalFailedFiles - totalSkippedFiles
+total_passed_files = total_file_count - total_failed - total_skipped
 code = 0
 
-if totalFiles == 0:
+if total_file_count == 0:
     code = 1
 
-if totalSkippedFiles > 0:
+if total_skipped > 0:
     code = 1
-    print("\n\nSkipped files (%d):" % totalSkippedFiles)
-    for warnFile in skippedFiles:
-        print("  - %s%s %s(%s)%s" % (fg('red'), warnFile[0], fg('yellow'), warnFile[1], attr(0)))
+    print("\n\nSkipped files (%d):" % total_skipped)
+    for warn_file in skipped_files:
+        print("  - %s%s %s(%s)%s" % (fg('red'), warn_file[0], fg('yellow'), warn_file[1], attr(0)))
 
-if totalFailedFiles > 0:
+if total_failed > 0:
     code = 2
-    print("\n\nFailed files (%d):" % totalFailedFiles)
-    for failedFile in failedFiles:
-        print("  - %s%s %s(%s)%s" % (fg('red'), failedFile[0], fg('yellow'), failedFile[1], attr(0)))
+    print("\n\nFailed files (%d):" % total_failed)
+    for failed_file in failed_files:
+        print("  - %s%s %s(%s)%s" % (fg('red'), failed_file[0], fg('yellow'), failed_file[1], attr(0)))
 
-totalPercent = 1
-if totalFiles > 0:
-    totalPercent = (totalPassedFiles / (totalFiles - totalSkippedFiles))
+# Calculate percentage total
+total_percent = 1
+if total_file_count > 0:
+    total_percent = (total_passed_files / (total_file_count - total_skipped))
+    total_percent = round(total_percent * 100, 2)
 
-if totalPercent > 0.8:
-    totalPercentColorStr = fg('green')
-elif totalPercent > 0.5:
-    totalPercentColorStr = fg('yellow')
+# Generate final output
+if total_percent > 0.8:
+    _total_percent_str = fg('green')
+elif total_percent > 0.5:
+    _total_percent_str = fg('yellow')
 else:
-    totalPercentColorStr = fg('red')
-
-resultStr = '\n\t'.join((
-    '\n Lint %s! (%s%s%s)' % (resultStr, totalPercentColorStr, str(round(totalPercent * 100, 2)) + "%", attr(0)),
-    'Passed: %s%d%s' % (fg('green') if totalPassedFiles > 0 else fg('light_gray'), totalPassedFiles, attr(0)),
-    'Failed: %s%d%s' % (fg('red') if totalFailedFiles > 0 else fg('light_gray'), totalFailedFiles, attr(0)),
-    'Skipped: %s%d%s' % (fg('yellow') if totalSkippedFiles > 0 else fg('light_gray'), totalSkippedFiles, attr(0)),
-))
-print(resultStr, end='')
-
+    _total_percent_str = fg('red')
+_result_color_str = _total_percent_str
+_total_percent_str = "%s%s%s" % (_result_color_str, str(total_percent) + "%", attr(0))
+_skipped_str = "%s%d%s skipped" % (fg('yellow') if total_skipped > 0 else fg('light_gray'), total_skipped, attr(0))
+_failed_str = "%s%d%s failed" % (fg('red') if total_failed > 0 else fg('light_gray'), total_failed, attr(0))
+_emoji = (fg('red') if total_failed > 0 else fg('green')) + ("✔" if total_failed == 0 else "✘" + attr(0))
+print('\n  %s Total (%s): %s / %s in %d files' % (
+    _emoji, _total_percent_str, _skipped_str, _failed_str, total_file_count), end='')
 sys.exit(code)
